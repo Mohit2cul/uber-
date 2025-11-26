d10f13074d671ee9dad8f25dc5a059b047cdd970
Mohit2cul <varshneykshitij3@gmail.com>
2025-11-26 19:08:46 +0530

    user registration done

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -22,3 +22,10 @@ module.exports.registerUser = async (req, res, next) => {
     const token = user.generateAuthToken();
     res.status(201).json({ user, token });
 }
+
+module.exports.loginUser = async (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+}
